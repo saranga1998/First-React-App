1fe2b165fad92a0d5acc7f4ffca0c35f915a9160
Sajitha Saranga <109761136+saranga1998@users.noreply.github.com>
2024-05-25 23:59:27 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import Greet from './Components/Greet'
+import Welcome from './Components/Welcome'
+
+function App() {
+  
+
+  return (
+    <div className='App'>
+      <Greet />
+    </div>
+  )
+}
+
+export default App
---
+++ src/Components/Greet.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+// export default function Greet() {
+//   return (
+//     <div>
+//       <h1>Hello world..</h1>
+//       <h1>Sajitha</h1>
+//     </div>
+//   );
+//}
+
+const Greet = () => <h1>Hi Saranga</h1>
+export default Greet
+
+
+//Functional Components -rfc
