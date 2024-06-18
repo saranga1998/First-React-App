36505d7dcb7321c33ad4e87f297f4306c93ef656
Sajitha Saranga <109761136+saranga1998@users.noreply.github.com>
2024-06-18 23:06:34 +0530

    PersonList

---
+++ src/Components/PersonList.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import Person from './Person'
+
+function PersonList() {
+    const Persons =[
+        {
+            id: 1,
+            name: 'Bruce',
+            age: 30,
+            skill: 'React'
+        },
+        {
+            id: 2,
+            name: 'Clark',
+            age: 25,
+            skill: 'Angular'
+        },
+        {
+            id: 3,
+            name: 'Diana',
+            age: 28,
+            skill: 'Vue'
+        }
+    ]
+
+    const personList = Persons.map(person => (<Person key={person.id} person={person}/>))
+  return (
+    <div>
+      {personList}
+    </div>
+  )
+}
+export default PersonList
