b0431ee3243cdab358744504191b16bbb172299b
Sajitha Saranga <109761136+saranga1998@users.noreply.github.com>
2024-06-23 23:26:07 +0530

    Test

--- src/Components/HoverCounterTwo.jsx
+++ src/Components/HoverCounterTwo.jsx
@@ -5,7 +5,7 @@ class HoverCounterTwo extends Component {
   render() {
     const {count,incrementCount} = this.props
     return (
-      <h2 onMouseOver={incrementCount}>Hoverd {count} times</h2>
+      <h2 onMouseOver={incrementCount}>Saranga Hoverd {count} times</h2>
     )
   }
 }
