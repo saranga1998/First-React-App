84a908f42a0ee950f94e4b23cabf7876434c6ef9
Sajitha Saranga <109761136+saranga1998@users.noreply.github.com>
2024-06-23 22:19:46 +0530

    HOC-2

--- src/Components/ClickCounter.jsx
+++ src/Components/ClickCounter.jsx
@@ -1,29 +1,18 @@
 import React, { Component } from 'react'
+import withCounter from './withCounter'
 
 class ClickCounter extends Component {
-    constructor(props) {
-      super(props)
     
-      this.state = {
-         count : 0
-      }
-    }
-
-    clickHandler = () => {
-      this.setState(prevState => {
-        return {count: prevState.count + 1}
-      })
-    }
     
   render() {
-    const {count} = this.state
+    const {count,incrementCount} = this.props
     
     return (
       <div>
-        <button onClick={this.clickHandler}>Click {count} times</button>
+        <button onClick={incrementCount}>{this.props.name} Click {count} times</button>
       </div>
     )
   }
 }
 
-export default ClickCounter
+export default withCounter(ClickCounter,5)
